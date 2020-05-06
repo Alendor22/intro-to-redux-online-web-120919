e794fc94be33232e20c2a477d929f404c944d71e
howardbdev <howarddevennish@gmail.com>
2020-05-06 08:12:29 -0400

    fix typo

--- comment.js
+++ comment.js
@@ -2,7 +2,7 @@ const Comment = ({name, content}) => {
   const commentsDiv = document.getElementById("comments-div")
   const commentBody = (
     `<div class="comment">
-      <h3${name} says:</h3>
+      <h3>${name} says:</h3>
       <p>${content}</p>
     </div>`
     )
